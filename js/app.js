a2270e3e4e06fcfa5f06dde9b4be003b0690f8f0
williamd <willy1divx@yahoo.com>
2017-02-03 09:49:33 +0000

    added styling

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var count = 0;
 var main = "main";
 var oneDeleteButton = 0;
 var opaque = 0;
+
 function addComment (){
 	var years = new Date();
 	var months = new Date();
@@ -16,6 +17,9 @@ function addComment (){
 	mint = fixMinute(minute);
 	date = month + " /" + day + "/" + year + "    Time: " + hour + ":" + mint; 
 	count += 1;
+	picture = Math.floor(Math.random() * 9) + 1;
+	images = 'img/' + picture + '.jpg';
+	$('#body').attr("background", images);
 	if ( count % 2 == 0){
 		// blue text
 	var userId = document.getElementById('userId').value;
@@ -31,7 +35,7 @@ function addComment (){
 	var h3 = document.createElement('h3');
 	var mainDiv = document.getElementById(divId);
 	mainDiv.appendChild(h3);
-	var uText = document.createTextNode("COMMENT: "+comment);
+	var uText = document.createTextNode(comment);
 	h3.appendChild(uText);
 	h3.className = "green"
 	var p = document.createElement('p');
@@ -41,7 +45,7 @@ function addComment (){
 	 p.className = "green"
 	 var datee = document.createElement('p');
 	 mainDiv.appendChild(datee);
-	 var cDate = document.createTextNode("DATE: "+date);
+	 var cDate = document.createTextNode("DATE: " + date);
 	 datee.appendChild(cDate);
 	 datee.className = "green"
 	 if (oneDeleteButton < 1 ){
@@ -53,8 +57,8 @@ function addComment (){
 		 deleteButton.setAttribute('id', main);
 		 oneDeleteButton = oneDeleteButton + 1
      }
-	 $('#'+main).click(function(){
-	 		$('#'+main+'div').remove();
+	 $('#' + main).click(function(){
+	 		$('#' + main + 'div').remove();
 	 		main = main - 1;
 	 		count = count - 1;
 	 		if (main == 0){
@@ -76,7 +80,7 @@ function addComment (){
 	var h3 = document.createElement('h3');
 	var mainDiv = document.getElementById(divId);
 	mainDiv.appendChild(h3);
-	var uText = document.createTextNode("COMMENT: "+comment);
+	var uText = document.createTextNode(comment);
 	h3.appendChild(uText);
 	var p = document.createElement('p');
 	mainDiv.appendChild(p);
@@ -93,10 +97,13 @@ function addComment (){
 	 deleteButton.appendChild(deleteText);
 	 deleteButton.className = 'white'
 	 deleteButton.setAttribute('id', main);
+	 // making sure to add a delete button
 	 oneDeleteButton = oneDeleteButton + 1;
 	}
-	 $('#'+main).click(function(){
-	 		$('#'+main+'div').remove();
+
+	//removing last comment
+	 $('#' + main).click(function(){
+	 		$('#' + main + 'div').remove();
 	 		main = main - 1;
 	 		count = count - 1;
 	 		if (main == 0){
@@ -136,4 +143,6 @@ function fixMinute(minute){
 	$("#hide").click(function(){
     $("#master").toggle();
 });
+
+
 
